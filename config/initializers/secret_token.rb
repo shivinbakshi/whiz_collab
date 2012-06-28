7d8ad448f217ff6e8dc1d85011b76dc3d968edfd
Shivin Bakshi <shivin_bakshi@hotmail.com>
2012-06-28 15:37:15 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhizCollab::Application.config.secret_token = '1364d303f281335558f56f23983cec13e81315d5ceb75b2671b9af418dd1f6157fb32e3663418f066e559ee2e3b9511d2c13ad5b9f53a6088f0d0529353db716'
