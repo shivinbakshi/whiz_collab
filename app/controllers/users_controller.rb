b25cdd4c1c5d8b86c92038af6b2099b305eac8ba
Shivin Bakshi <shivin_bakshi@hotmail.com>
2012-07-20 15:10:00 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_filter :admin_user,     only: :destroy
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,7 +39,9 @@ describe User do
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
   it { should respond_to(:remember_token)}
+  it { should respond_to(:feed) }
 
   it { should be_valid }
 
@@ -123,4 +125,38 @@ describe "with a password that's too short" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts
+      @user.destroy
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+  end
+
 end
